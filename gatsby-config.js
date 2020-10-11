b2cfac3e85ef97a5b44ef01f808d417c86af6077
Nicolas dos Santos <nicolas.dossantos@lmco.com>
2020-10-10 21:21:28 -0400

    Add mdx and sharp library plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,25 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`
       }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            }
+          }
+        ]
+       
+        
+      }
+
     }
 
   ],
